37add51ac1b92af4ef5c1531b75d88892211ff85
Mohammad <m.hussin@student.alfa-college.nl>
2025-11-14 12:26:22 +0100

    dit is de laatste versie

--- Python map/Percentage_programmatje.py
+++ Python map/Percentage_programmatje.py
@@ -1,11 +1,13 @@
 
 print("Welk type procentsom wil je oplossen; maak je keuze:")
-print("[1] Hoeveel is x% van y? ")
-print("[2] Hoeveel % is x van y? ")
-print("[3] Hoeveel % neemt toe van y? ")
-print("[4] Hoeveel % neemt af van y? ")
-print("[5] Hoeveel y toegenomen naar x? ")
-print("[6] Hoeveel is y toegenomen met x%? ")
+print("[1] Percentage van een getaal berekenen ")
+print("[2] Welke percentage is x van y? ")
+print("[3] Toename van precentage ")
+print("[4] Bedrag na procentuele afname ")
+print("[5.1] Met hoeveel is iets gestegen?  ")
+print("[5.2] Met heoveel procent is iets gedaald?  ")
+print("[6.1] Bedrag voor een precentuele verhoging ")
+print("[6.2] Bedrag voor een procentuele verlaging ")
 
 choice = input("Welke optie kies je?")
 match choice:
@@ -16,7 +18,7 @@ match choice:
         print("De vraag is: hoeveel is " + str(x) + "% van " + str(y) + "?")
         print("Het antwoord is: " + str(answer))
         print("De berekening is: " + str(x) + "% = " + str(x/100) + " en " + str(x/100) + " x " + str(y) + " = " + str(answer))
-        print(f"De berekening is: \n {x} : {y} = {x / y}; {x / y} x 100 = {int(round(answer, 0))}%")
+        print(f"De berekening is: \n {x} : {y} = {x / y}; {x / y} x 100 = {int(round(answer, 0))}")
     case "2":
         x = float(input("Wat is x?"))
         y = float(input("Wat is y?"))
@@ -36,18 +38,28 @@ match choice:
         answer = (((100 - x) * y) / 100)
         print(f"De berekening is: \n 100 - x = {100 - x}; x = {x * y} : {x / 100}; = {float(answer)}")
 
-    case "5":
+    case "5.1":
+
         x = float(input("wat is x? "))
         y = float(input("Wat is y? "))
-        answer1 = ((x / y) - 1) * 100
-        # answer2 = ((y * 100) / x)
-        print(f"De berekening is: x = {x / y}; {x - 1}; {x * 100} = {float(input(round(answer1, 0)))}%")
-        # print(f"De berekning is: x= {y * 100}; {y / x}; {100 - y} = {float(input(round(answer2, 0)))}%")
+        answer = ((x / y) - 1) * 100
+        print(f"De berekening is: x = {x / y}; {x - 1}; {x * 100} = {float(input(round(answer, 0)))}%")
 
-    case "6":
+    case "5.2":
+        x = float(input("wat is x? "))
+        y = float(input("wat is y? "))
+        answer = ((y * 100) / x)
+        print(f"De berekning is: x = {y * 100}; {y / x}; {100 - y} = {float(input(round(answer, 0)))}")
+
+    case "6.1":
         x = float(input("Wat is x? "))
         y = float(input("Wat is y? "))
-        answer1 = ((x * 100) / (y + 100))
-        # answer2 = ((x * 100) / y)
-        print(f"De berekning is: x = {x * 100}; {x / y} = {float(input(round(answer1, 0)))}")
-        # print(f"De berekning is: x = {x * 100}; {x / y} = {float(input(round(answer2, 0)))}")
+        answer = ((x * 100) / (y + 100))
+        print(f"De berekning is: x = {x * 100}; {x / y} = {float(input(round(answer, 0)))}")
+
+
+    case "6.2":
+        x = float(input("Wat is x? "))
+        y = float(input("Wat is y? "))
+        answer = ((x * 100) / y)
+        print(f"De berkening is: x = {x * 100}; {x / y} = {float(input(round(answer, 0)))}")
