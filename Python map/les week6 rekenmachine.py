90a45f398341998285150d4cebadc5b6fca7fe29
Mohammad <m.hussin@student.alfa-college.nl>
2025-11-14 14:19:52 +0100

    dit is de laatste versie

--- Python map/les week6 rekenmachine.py
+++
@@ -1,6 +0,0 @@
-# Schrijf een rekenmachine met de volgende functies: optellen, aftrekken en vermenigvuldigen.
-#  Geef de gebruiker een keuzemenu om een van deze bewerkingen te selecteren en voer de berekening uit op twee door de gebruiker ingevoerde getallen. 
-# Zetde bewerkingsopties als aparte functies met een return statement.
-#  Zorg dat het programma via een keuzemenu door blijft lopen tenzij er voor stoppen word gekozen.
-
-import os  
